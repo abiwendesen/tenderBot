f723f42be00e0b2ab2df16463792903d800bfa7f
abi <abiwendesen21@gmail.com>
2026-03-25 22:00:57 +0300

    implementing the insertTender func

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ const check =  await embedd('africa union has large building in addis ababa')
     wait: true,
     points:[
         {id: 1 , vector:[0.71, 0.33, 0.64, 0.12], payload : {city: "Addis Ababa is the heart of africa" }},
-        {id: 2 , vector:[0.41, 0.22, 0.35, 0.19],payload : {city: "Melbourne just a city in Australia aussies love it but not that much of city "}},
+        {id: 2 , vector:[0.41, 0.22, 0.35, 0.19],payload : {city: "Melbourne is just a city in Australia, aussies love it, but not that much of city "}},
         { id: 3, vector: [0.63, 0.48, 0.72, -0.15], payload: { city: "Moscow the land of lenin and stali, you could say the birth place of socializm" } }
     ]
 })
--- vectorStrore.js
+++ vectorStrore.js
@@ -3,3 +3,26 @@ import { BGE_DIMS,embedd } from "./utill/embedder";
 
 
 
+export async function insertTender(tender) {
+    const text = [tender.lotName, tender.lotDescription].join(' ').trim();
+
+    if(!text)return;
+
+    const vector = await embedd(text);
+
+    await client.upsert('test_collection',
+        {
+            wait:true,
+            points:[
+                {
+                    id: tender.id,
+                    vector,
+                    payload:{
+                        
+                    }
+                }
+            ]
+        }
+    )
+    
+}
