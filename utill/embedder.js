97ca95c24ffc1e2644819716d8dc2ac1da9a2003
abi <abiwendesen21@gmail.com>
2026-03-28 15:56:19 +0300

    singel insert is taking too long need to implement batch insert

--- utill/embedder.js
+++ utill/embedder.js
@@ -1,6 +1,6 @@
 import { pipeline } from "@xenova/transformers";
 
-export const BGE_DIMS = 384;
+export const BGE_DIMS = 1024;
 export const embedd = async(string)=>{
     const extaractor = await pipeline('feature-extraction',
          "Xenova/bge-m3",{quantized: true});
@@ -8,7 +8,7 @@ export const embedd = async(string)=>{
     const embedding = await extaractor(string,
         {pooling:'cls', normalize:true}
     )
-    // console.log("embedded")
+     console.log("embedded")
 
     return  Array.from(embedding.data);
 }
